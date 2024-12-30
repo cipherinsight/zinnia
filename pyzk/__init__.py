b83ea90e9f3ca324d5283b7c44a23be0a4894903
zhantong <listerily@outlook.com>
2024-12-30 20:12:01 +0800

    refactored interface

--- pyzk/__init__.py
+++ pyzk/__init__.py
@@ -1,4 +1,4 @@
-from .pyzk_interface import pyzk_circuit
+from .pyzk_interface import pyzk_circuit, pyzk_chip, PyZKChip, PyZKCircuit
 from .lang.type import Integer, Float, Tuple, NDArray
 from .lang.typing import Public, Private
 from .lang.operator import add, sub, div, mul, sin, cos, tan, sinh, cosh, tanh, exp, log, concatenate, stack
--- pyzk/pyzk_interface.py
+++ pyzk/pyzk_interface.py
@@ -15,50 +15,81 @@ from pyzk.debug.prettifier import prettify_zk_ast, prettify_ir_stmts, prettify_e
 from pyzk.exec.exec_ctx import ExecutionContext
 from pyzk.exec.executor import Halo2ZKProgramExecutor
 
-def pyzk_circuit(method, debug=True):
-    def __inner(*args, **kwargs):
-        source_code = inspect.getsource(method)
-        tree = ast.parse(source_code)
-        defined_chips: Dict[str, ChipObject] = {}
-        for key, val in inspect.currentframe().f_back.f_locals.items():
-            if isinstance(val, ChipObject):
-                defined_chips[key] = val
+
+class PyZKChip:
+    def __init__(self, name: str, source: str):
+        self.name = name
+        self.source = source
+        tree = ast.parse(self.source)
+        try:
+            transformer = PyZKChipASTTransformer(self.source, self.name)
+            ir_comp_tree = transformer.visit(tree.body[0])
+        except InternalPyzkException as e:
+            raise prettify_exception(e)
+        self.chip = ChipObject(ir_comp_tree)
+
+    def get_chip(self) -> ChipObject:
+        return self.chip
+
+
+class PyZKCircuit:
+    def __init__(self, name: str, source: str, chips: Dict[str, PyZKChip | ChipObject], debug=False):
+        self.name = name
+        self.source = source
+        self.chips = chips
+        self.debug = debug
+        tree = ast.parse(self.source)
         try:
             if debug:
                 print('*' * 20 + ' Original AST ' + '*' * 20, file=sys.stderr)
                 print(astpretty.pformat(tree.body[0], show_offsets=True), file=sys.stderr)
-            transformer = PyZKCircuitASTTransformer(source_code, method.__name__, defined_chips)
+            transformer = PyZKCircuitASTTransformer(
+                self.source, self.name,
+                {key: (chip.get_chip() if isinstance(chip, PyZKChip) else chip) for key, chip in self.chips}
+            )
             ir_comp_tree = transformer.visit(tree.body[0])
             if debug:
                 print('*' * 20 + ' Transformed AST ' + '*' * 20, file=sys.stderr)
                 print(prettify_zk_ast(ir_comp_tree), file=sys.stderr)
             generator = IRGenerator()
-            ir_stmts, prog_metadata = generator.generate(ir_comp_tree)
+            ir_stmts, self.prog_metadata = generator.generate(ir_comp_tree)
             if debug:
                 print('*' * 20 + ' IR Statements ' + '*' * 20, file=sys.stderr)
                 print(prettify_ir_stmts(ir_stmts), file=sys.stderr)
                 print('*' * 20 + ' Program Metadata ' + '*' * 20, file=sys.stderr)
-                print(json.dumps(prog_metadata.export()), file=sys.stderr)
-            prog_metadata.set_circuit_name(method.__name__)
-            prog_builder = Halo2ProgramBuilder(ir_stmts, prog_metadata)
-            zk_program = prog_builder.build()
+                print(json.dumps(self.prog_metadata.export()), file=sys.stderr)
+            self.prog_metadata.set_circuit_name(self.name)
+            prog_builder = Halo2ProgramBuilder(ir_stmts, self.prog_metadata)
+            self.zk_program = prog_builder.build()
             if debug:
                 print('*' * 20 + ' Program Source ' + '*' * 20, file=sys.stderr)
-                print(zk_program.source, file=sys.stderr)
-            exec_ctx = ExecutionContext(prog_metadata, args, kwargs)
-            executor = Halo2ZKProgramExecutor(exec_ctx, zk_program)
+                print(self.zk_program.source, file=sys.stderr)
+        except InternalPyzkException as e:
+            raise prettify_exception(e)
+
+    def __call__(self, *args, **kwargs):
+        try:
+            exec_ctx = ExecutionContext(self.prog_metadata, args, kwargs)
+            executor = Halo2ZKProgramExecutor(exec_ctx, self.zk_program)
             executor.exec()
         except InternalPyzkException as e:
             raise prettify_exception(e)
+
+
+def pyzk_circuit(method, debug=True):
+    def __inner(*args, **kwargs):
+        source_code = inspect.getsource(method)
+        method_name = method.__name__
+        defined_chips: Dict[str, ChipObject] = {}
+        for key, val in inspect.currentframe().f_back.f_locals.items():
+            if isinstance(val, ChipObject):
+                defined_chips[key] = val
+        circuit = PyZKCircuit(method_name, source_code, defined_chips, debug)
+        return circuit(*args, **kwargs)
     return __inner
 
 
 def pyzk_chip(method):
     source_code = inspect.getsource(method)
-    tree = ast.parse(source_code)
-    try:
-        transformer = PyZKChipASTTransformer(source_code, method.__name__)
-        ir_comp_tree = transformer.visit(tree.body[0])
-    except InternalPyzkException as e:
-        raise prettify_exception(e)
-    return ChipObject(ir_comp_tree)
+    method_name = method.__name__
+    return PyZKChip(method_name, source_code).get_chip()
