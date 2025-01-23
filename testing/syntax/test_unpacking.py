fc5ae466f88595d01337ee3b0a13cd35896a9ad5
zhantong <listerily@outlook.com>
2025-01-23 15:52:09 +0800

    more unpacking tests

--- testing/syntax/test_unpacking.py
+++ testing/syntax/test_unpacking.py
@@ -108,3 +108,69 @@ def test_tuple_unpacking_with_inner():
         assert e == 5
 
     ZKCircuit.from_method(foo).compile()
+
+
+def test_tuple_unpacking_with_inner_and_star():
+    """This test is to check if the tuple unpacking with star is done correctly"""
+    @zk_circuit
+    def foo():
+        tup = (1, (2, 3, 3), 4, 5)
+        a, (b, *c), *d = tup
+        assert a == 1
+        assert b == 2
+        assert c == [3, 3]
+        assert d == [4, 5]
+
+    ZKCircuit.from_method(foo).compile()
+
+
+def test_list_unpacking():
+    """This test is to check if the tuple unpacking with star is done correctly"""
+    @zk_circuit
+    def foo():
+        lst = [1, 2, 3]
+        a, b, c = lst
+        assert a == 1
+        assert b == 2
+        assert c == 3
+
+    ZKCircuit.from_method(foo).compile()
+
+
+def test_list_unpacking_with_square_brackets():
+    """This test is to check if the tuple unpacking with star is done correctly"""
+    @zk_circuit
+    def foo():
+        lst = [1, 2, 3]
+        [a, b, c] = lst
+        assert a == 1
+        assert b == 2
+        assert c == 3
+
+    ZKCircuit.from_method(foo).compile()
+
+
+def test_list_unpacking_with_star():
+    """This test is to check if the tuple unpacking with star is done correctly"""
+    @zk_circuit
+    def foo():
+        lst = [1, 2, 3, 4]
+        a, *b, c = lst
+        assert a == 1
+        assert b == [2, 3]
+        assert c == 4
+
+    ZKCircuit.from_method(foo).compile()
+
+
+def test_swap_by_unpacking():
+    """This test is to check if the tuple unpacking with star is done correctly"""
+    @zk_circuit
+    def foo():
+        a = 1
+        b = 2
+        a, b = b, a
+        assert a == 2
+        assert b == 1
+
+    ZKCircuit.from_method(foo).compile()
