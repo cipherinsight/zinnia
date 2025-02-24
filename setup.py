c33916dd2c0572bad69781c71d686cc334e91e0c
Pingchuan Ma <44944354+pckennethma@users.noreply.github.com>
2025-02-24 12:39:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 package = ["zinnia"]
 version = "0.0.1"
 description = (
-    "A tool for synthesizing discrete data-generating processes from noisy I/O examples"
+    "A compiler for compiling Python code to low-level zero-knowledge circuit."
 )
 author = "CipherInsight Ltd."
 
