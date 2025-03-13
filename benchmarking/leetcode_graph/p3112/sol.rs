e5c96d98111f928c407d7297023b0f83603a4083
--global <--global>
2025-03-13 23:05:11 +0800

    update results

--- benchmarking/leetcode_graph/p3112/sol.rs
+++ benchmarking/leetcode_graph/p3112/sol.rs
@@ -26,8 +26,8 @@ const R_P: usize = 57;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct CircuitInput {
-    pub graph: Vec<u128>,
-    pub disappear: Vec<u128>,
+    pub graph: Vec<i128>,
+    pub disappear: Vec<i128>,
     pub answers: Vec<i128>,
 }
 
@@ -47,13 +47,19 @@ fn verify_solution<F: ScalarField>(
     // load graph
     let mut graph: Vec<AssignedValue<F>> = Vec::new();
     for i in 0..100 {
-        let value = ctx.load_witness(F::from_u128(input.graph[i]));
+        let mut value = ctx.load_witness(F::from_u128((input.graph[i]).abs() as u128));
+        if input.graph[i] < 0 {
+            value = GateInstructions::neg(&gate, ctx, value);
+        }
         graph.push(value);
     }
     // load disappear
     let mut disappear: Vec<AssignedValue<F>> = Vec::new();
     for i in 0..10 {
-        let value = ctx.load_witness(F::from_u128(input.disappear[i]));
+        let mut value = ctx.load_witness(F::from_u128((input.disappear[i]).abs() as u128));
+        if input.disappear[i] < 0 {
+            value = GateInstructions::neg(&gate, ctx, value);
+        }
         disappear.push(value);
     }
     // load answers
@@ -65,7 +71,7 @@ fn verify_solution<F: ScalarField>(
             answers.push(value);
 
         } else {
-            let value = ctx.load_constant(F::from_u128(input.answers[i] as u128));
+            let value = ctx.load_witness(F::from_u128(input.answers[i] as u128));
             answers.push(value);
         }
     }
@@ -97,13 +103,14 @@ fn verify_solution<F: ScalarField>(
     for i in 0..10 {
         let graph_0_i = graph[i];
         let graph_0_i_equals_negative_one = gate.is_equal(ctx, graph_0_i, negative_one);
+        let graph_0_i_not_equals_negative_one = gate.not(ctx, graph_0_i_equals_negative_one);
         let answer_equals_negative_one = gate.is_equal(ctx, answers[i], negative_one);
         let answer_i_equals_graph_0_i = gate.is_equal(ctx, graph_0_i, answers[i]);
         // apply constraint #1
         let disappear_i_equal_graph_0_i: AssignedValue<F> = gate.is_equal(ctx, disappear[i], graph_0_i);
         let disappear_i_gt_graph_0_i: AssignedValue<F> = range_chip.is_less_than(ctx, graph_0_i, disappear[i], 128);
         let disappear_i_gte_graph_0_i = gate.or(ctx, disappear_i_gt_graph_0_i, disappear_i_equal_graph_0_i);
-        let cond = gate.and(ctx, disappear_i_gte_graph_0_i, graph_0_i_equals_negative_one);
+        let cond = gate.and(ctx, disappear_i_gte_graph_0_i, graph_0_i_not_equals_negative_one);
         let cond_not = gate.not(ctx, cond);
         let constraint = gate.or(ctx, cond_not, answer_i_equals_graph_0_i);
         gate.assert_is_const(ctx, &constraint, &F::ONE);
